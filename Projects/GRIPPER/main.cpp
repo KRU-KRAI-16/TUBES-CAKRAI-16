2dd047d6a0beb5a660fea88f429120a678075058
ElektrikHandal <151951675+ElektrikHandal@users.noreply.github.com>
2024-08-13 17:31:51 +0700

    Gripper coy

--- Projects/GRIPPER/main.cpp
+++ Projects/GRIPPER/main.cpp
@@ -1,13 +1,130 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2024 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
+#include "../../KRAI_library/KRAI_library/Pinout/BoardManagerV1.h"
+#include "../../KRAI_library/Motor/Motor.h"
+#include "../../KRAI_library/encoderKRAI/encoderKRAI.h"
+#include "../../KRAI_library/KRAI_library/MiniPID/MiniPID.h"
+#include "../../KRAI_library/servoKRAI/servoKRAI.h"
+#include ".../../KRAI_library/KRAI_library/CanBusKRAI/BMAktuatorKRAI.hpp"
 
+//============================SETUP BASIC TIMER======================================
+Ticker ms_tick;
+uint32_t millis = 0;
+void onMillisecondTicker(void)
+{
+    // this code will run every millisecond
+    millis++;
+}
+DigitalOut led(PC_13);
+//-----------------------------------------------------------------------------------
+//=========================SETUP UART SERIAL PRINT===================================
+#define SERIAL_TX PB_6
+#define SERIAL_RX PB_7
+#define SERIAL_BAUDRATE 115200
 
+static BufferedSerial serial_port(SERIAL_TX, SERIAL_RX, SERIAL_BAUDRATE);
+FileHandle *mbed::mbed_override_console(int fd) {
+    return &serial_port;
+}
+//-----------------------------------------------------------------------------------
+
+//==============================SETUP CANBUS==========================================
+#define CAN_TX PA_11
+#define CAN_RX PA_12
+#define ID_BM 7
+
+int data_timer = 0;
+int can_timeout_timer = 0;
+CAN can(PA_11, PA_12, 500000);
+
+BMAktuatorKRAI BoardModular(ID_BM, &millis);
+
+// Definisikan Lamanya Menerima data
+#define TS_READ_CAN     2   // 
+#define TS_SEND_CAN     5   // 
+//-----------------------------------------------------------------------------------
+
+// ENCODER
+#define PPR 10332.0f
+
+// PID
+MiniPID pid(0.06, 0, 0);
+uint32_t last = 0; 
+uint32_t lastposition = 0;
+ 
 int main()
 {
-    printf("Hello, Mbed!\n");
+    //TIMER
+    ms_tick.attach(&onMillisecondTicker, 0.001); 
+
+    //SERVO
+    servoKRAI myServo(BMV1_INT_1);
+
+    //MOTOR
+    Motor motor(BMV1_PWM_MOTOR_2, BMV1_FOR_MOTOR_2, BMV1_REV_MOTOR_2);
+    encoderKRAI enc(BMV1_ENCODER_1_A, BMV1_ENCODER_1_B, PPR, Encoding::X4_ENCODING);
+
+    //PID
+    float derajat;
+    pid.setOutputLimits(-1.0, 1.0);
+    float pidOutput;
+    float targetPosition = 0.0; // Start at 0 degrees
+    
+    while (true)
+    {
+        //////////////////////////////////////////////////////////////////
+        // Update PID every 10 ms
+        // if (millis - last >= 10){
+        //     derajat = ((enc.getPulses() * 360.0f) / PPR); // Calculate current position in degrees
+        //     float pidOutput = pid.getOutput(derajat, targetPosition); // Control to the current target position
+        //     motor.speed(pidOutput); // Set motor speed based on PID output
+        //     last = millis; // Update the last update time
+        // }
+
+        // // Change the target position every 5 seconds
+        // if (millis - lastposition >= 5000){
+        //     if (targetPosition == 0.0){
+        //         targetPosition = 100.0; // Move to 100 degrees
+        //     } else if (targetPosition == 100.0){
+        //         targetPosition = 0.0; // Move back to 0 degrees
+        //     }
+        //     lastposition = millis; // Update the last position change time
+        // }
+
+        //////////////////////////////////////////////
+        //     // control motor speed pake outputpid
+        //     if (millis - last > 5000) {
+        //         float pidOutput = pid.getOutput(currentPosition, 0);
+        //         motor.speed(pidOutput);
+        //         last = millis;
+        //     }
+
+        // TERIMA DATA CAN
+        if (BoardModular.readCAN(100)){
+            
+
+        }
+        //  -----------------------
+        if (millis - last >= 10){
+            derajat = ((enc.getPulses() * 360.0f) / PPR); // dapat posisi derajat motor
+            pidOutput = pid.getOutput(derajat, targetPosition); // pid out
+            motor.speed(pidOutput); // set motor
+            last = millis; // waktu
+        }
+
+        if (millis - lastposition>= 5000){
+            myServo.position(80);
+            if(millis-lastposition >= 7000){
+                targetPosition = 100.0; 
+                if (derajat >= 97.0){
+                    myServo.position(-20);
+                    if (millis - lastposition >= 10000){
+                        targetPosition = 0.0; 
+                        lastposition = millis;
+                    }
+                }
+            }
+        }
+        printf("derajat = %f \n ", (derajat));
+    }
     return 0;
 }
