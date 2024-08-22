0de824f1f023141adfd36ee9f4f2dbce4edfdb9f
karolyangqian <karolyangqian14@gmail.com>
2024-08-22 12:35:37 +0700

    Test all features

---
+++ Projects/Base_Kanan/main.cpp
@@ -0,0 +1,205 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2024 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+// ========================LIBRARIES==========================
+// BOARD MANAGER
+#include "../../../KRAI_library/Pinout/BoardManagerV1.h"
+#include "../../../KRAI_library/CanBusKRAI/BMAktuatorKRAI.hpp"
+
+// MOTOR AND ENCODER
+#include "../../../KRAI_library/Motor/Motor.h"
+#include "../../../KRAI_library/encoderKRAI/encoderKRAI.h"
+
+// TEMP PID
+#include "../../../KRAI_library/MiniPID/MiniPID.h"
+#include "../../../KRAI_library/MovingAverage/MovingAverage.h"
+
+// ADRC V2
+#include "../../../KRAI_library/ADRC_V2/ADRC_V2.h"
+
+//--------------------------------------------------------------
+
+
+// DECLARATION MOTORS
+// BR = MOTOR 1
+// FR = MOTOR 2
+
+#define PWM_BR BMV1_PWM_MOTOR_1
+#define FOR_BR BMV1_FOR_MOTOR_1
+#define REV_BR BMV1_REV_MOTOR_1
+
+#define PWM_FR BMV1_PWM_MOTOR_2
+#define FOR_FR BMV1_FOR_MOTOR_2
+#define REV_FR BMV1_REV_MOTOR_2
+
+#define CHA_BR BMV1_ENCODER_1_A
+#define CHB_BR BMV1_ENCODER_1_B
+
+#define CHA_FR BMV1_ENCODER_2_A
+#define CHB_FR BMV1_ENCODER_2_B
+
+#define PPR 537.6f
+
+// Object Motor - Encoder
+Motor motor_BR(PWM_BR, FOR_BR , REV_BR);
+Motor motor_FR(PWM_FR, FOR_FR , REV_FR);
+
+encoderKRAI encoder_BR(CHB_BR , CHA_BR , PPR , Encoding::X4_ENCODING);
+encoderKRAI encoder_FR(CHB_FR , CHA_FR , PPR , Encoding::X4_ENCODING);
+
+MovingAverage movAvgBR(10);
+MovingAverage movAvgFR(10);
+
+// PID SEMENTARA
+// PID params
+// double Kp = 0.10f;
+// double Ki = 0.0034f;
+// double Kd = 0.04f;
+
+// PIDs
+// MiniPID pid_BR(Kp, Ki, Kd);
+// MiniPID pid_FR(Kp, Ki, Kd);
+
+// ADRC V2
+// Parameter for normal speed
+uint32_t timeSampling = 7;
+
+float b0_FrontMotor_normal = 430;
+float b0_BackMotor_normal = 430;
+float tSettle_normal = 0.5;
+float zESO_normal = 7.25;
+float incrementInt_normal = 0.09;
+
+ADRC_V2 ADRC_BR(timeSampling / 1000.0f, b0_FrontMotor_normal, tSettle_normal, zESO_normal, incrementInt_normal);
+ADRC_V2 ADRC_FR(timeSampling / 1000.0f, b0_BackMotor_normal, tSettle_normal, zESO_normal, incrementInt_normal);
+
+
+//============================SETUP BASIC TIMER======================================
+Ticker ms_tick;
+uint32_t millis = 0;
+void onMillisecondTicker(void)
+{
+    // this code will run every millisecond
+    millis++;
+}
+
+// SETUP TIMER IN US_TICKER_READ
+// US = µ Second -> Timer dengan satuan detik 10^6 (µ)
+int ms_ticker_read(){
+
+    return us_ticker_read()/1000;
+
+}
+//-----------------------------------------------------------------------------------
+
+DigitalOut led(PC_13);
+
+//=========================SETUP UART SERIAL PRINT===================================
+#define SERIAL_TX PB_6
+#define SERIAL_RX PB_7
+#define SERIAL_BAUDRATE 115200
+
+static BufferedSerial serial_port(SERIAL_TX, SERIAL_RX, SERIAL_BAUDRATE);
+FileHandle *mbed::mbed_override_console(int fd) {
+    return &serial_port;
+}
+//-----------------------------------------------------------------------------------
+
+//==============================SETUP CANBUS==========================================
+#define CAN_TX PA_11
+#define CAN_RX PA_12
+#define ID_BM_BASE_KANAN 1
+int data_timer = 0;
+int can_timeout_timer = 0;
+CAN can(CAN_TX, CAN_RX, 500000);
+
+/* 
+getMotor1() -> BR
+gedtMotor2() -> FR
+*/
+
+BMAktuatorKRAI BM_Base(ID_BM_BASE_KANAN, &millis);
+
+// Definisikan Lamanya Menerima data
+#define TS_READ_CAN     2   // 
+#define TS_SEND_CAN     5   // 
+//-----------------------------------------------------------------------------------
+#define ANALOG_SCALE 1000.0f
+
+int main()
+{
+    //=========================INITIALIZATION====================================
+
+    ms_tick.attach_us(onMillisecondTicker,1000);
+
+    float BR_setvalue, FR_setvalue;
+
+    // PID SEMENTARA
+    //float BR_speed, FR_speed;
+
+    float pulseThen_BR = encoder_BR.getPulses();
+    float pulseThen_FR = encoder_FR.getPulses();
+    float lastmillispulse = millis;
+    float rotatePerSec_BR;
+    float rotatePerSec_FR;
+    float PWM_motor_BR;
+    float PWM_motor_FR;
+
+
+    // pid_BR.setOutputLimits(-1,1);
+    // pid_FR.setOutputLimits(-1,1);
+    
+    // --------------------------------------------------------------------------
+
+    while (true)
+    {
+        //============================USER CODE==================================
+
+        if(BM_Base.readCAN(TS_READ_CAN)){
+            if (millis - data_timer > 500)
+            {
+                led = !led;
+                data_timer = millis;
+            }
+
+            can_timeout_timer = millis;
+        }
+
+        BR_setvalue = BM_Base.getMotor1();
+        BR_setvalue = BR_setvalue / ANALOG_SCALE;
+        FR_setvalue = BM_Base.getMotor2();
+        FR_setvalue = FR_setvalue / ANALOG_SCALE;
+
+        // printf("mot1 : %f, mot2 : %f \n", BR_setvalue, FR_setvalue);
+
+        if (millis - lastmillispulse > timeSampling){
+            lastmillispulse = millis;
+            rotatePerSec_BR = (encoder_BR.getPulses() - pulseThen_BR)/(PPR*0.01);
+            rotatePerSec_FR = (encoder_FR.getPulses() - pulseThen_FR)/(PPR*0.01);
+
+            rotatePerSec_BR = movAvgBR.movingAverage(rotatePerSec_BR);
+            rotatePerSec_FR = movAvgFR.movingAverage(rotatePerSec_FR);
+
+            pulseThen_BR = encoder_BR.getPulses();
+            pulseThen_FR = encoder_FR.getPulses();
+            
+            PWM_motor_BR = ADRC_BR.createInputSignal(ADRC_BR.fhan_setPointTrajectory(BR_setvalue, 0.6), rotatePerSec_BR, 1.0f);
+            PWM_motor_FR = ADRC_FR.createInputSignal(ADRC_FR.fhan_setPointTrajectory(FR_setvalue, 0.6), rotatePerSec_FR, 1.0f);
+
+            // PID SEMENTARA
+            // PWM_motor_BR = pid_BR.getOutput(rotatePerSec_BR, BR_setvalue);
+            // PWM_motor_FR = pid_FR.getOutput(rotatePerSec_FR, FR_setvalue);
+        }
+
+        // ----------------------------------------------------------------------
+        motor_BR.speed(PWM_motor_BR);
+        motor_FR.speed(PWM_motor_FR);
+    }
+    
+    
+    return 0;
+}
---
+++ Projects/Master_Board/main.cpp
@@ -0,0 +1,337 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2024 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+// Board Manager
+#include "../../../KRAI_library/Pinout/BoardManagerV1.h"
+#include "../../../KRAI_library/CanBusKRAI/BMAktuatorKRAI.hpp"
+
+// Joystick
+#include "../../../KRAI_library/JoystickPS3/JoystickPS3.h"
+
+// Inverse Kinematics
+#include "../../../KRAI_library/InverseKinematics/Omni4Wheel.h"
+
+//=========================SETUP BASIC TIMER======================================
+Ticker ms_tick;
+uint32_t millis = 0;
+void onMillisecondTicker(void)
+{
+    // this code will run every millisecond
+    millis++;
+}
+
+// SETUP TIMER IN US_TICKER_READ
+// US = µ Second -> Timer dengan satuan detik 10^6 (µ)
+int ms_ticker_read(){
+
+    return us_ticker_read()/1000;
+
+}
+
+DigitalOut led(PC_13);
+
+//-----------------------------------------------------------------------------------
+
+//=========================SETUP UART SERIAL PRINT===================================
+#define SERIAL_TX PB_6
+#define SERIAL_RX PB_7
+#define SERIAL_BAUDRATE 115200
+
+static BufferedSerial serial_port(SERIAL_TX, SERIAL_RX, SERIAL_BAUDRATE);
+FileHandle *mbed::mbed_override_console(int fd) {
+    return &serial_port;
+}
+//-----------------------------------------------------------------------------------
+
+//==============================SETUP CANBUS==========================================
+#define CAN_TX PD_0
+#define CAN_RX PD_1
+#define ID_BM_BASE_KANAN 1
+#define ID_BM_BASE_KIRI 4
+#define ID_BM_GRIPPER 3
+#define ID_BM_STORAGE 5
+
+int data_timer = 0;
+int can_timeout_timer = 0;
+CAN can(PA_11, PA_12, 500000); //Cek lagi pin Transmit Receive di Board 1
+
+BMAktuatorKRAI can_kanan (ID_BM_BASE_KANAN, &millis);
+BMAktuatorKRAI can_kiri (ID_BM_BASE_KIRI, &millis);
+BMAktuatorKRAI gripper (ID_BM_GRIPPER, &millis);
+BMAktuatorKRAI storage (ID_BM_STORAGE, &millis);
+
+// Definisikan Lamanya Menerima dan kirim data
+#define TS_READ_CAN     2   // 
+#define TS_SEND_CAN     5   // 
+//-----------------------------------------------------------------------------------
+
+//==============================SETUP INVERSE KINEMATICS=============================
+
+#define SPEED_CONST 2.0f
+#define OMEGA_CONST 0.5f
+#define ANALOG_SCALE 1000.0f
+unsigned long* millisPtr;
+
+Omni4Wheel omniwheel(millisPtr, 0.395, 0.15);
+
+//-----------------------------------------------------------------------------------
+
+//==============================UART PS3=============================================
+JoystickPS3 ps3 (BMV1_UART_TX,BMV1_UART_RX);
+class TimeOutPS3
+{
+private:
+    uint32_t prevTime = 0;
+    uint32_t tempIntegral = 0;
+    uint32_t durasiIgnore = 200; // ms
+public:
+    TimeOutPS3() = default;
+    TimeOutPS3(uint32_t durasiIgn) : durasiIgnore(durasiIgn) {}
+    void updateTime(uint32_t time) { this->prevTime = time; }
+    bool checkTimeOut(bool run, uint32_t timeNow) {
+        if (run == false) {
+            this->tempIntegral = 0;
+            this->prevTime = timeNow;
+            return false;
+        }
+        else {
+            this->tempIntegral +=  (timeNow - this->prevTime);
+            if (this->tempIntegral > this->durasiIgnore) {
+                return true;
+            }
+            return false;
+        }
+    }
+};
+
+//INISIASI TIMEOUT//
+TimeOutPS3 timeOut_R1(200);
+TimeOutPS3 timeOut_L1(200);
+TimeOutPS3 timeOut_Atas(200);
+TimeOutPS3 timeOut_Bawah(200);
+TimeOutPS3 timeOut_Kanan(200);
+TimeOutPS3 timeOut_Kiri(200);
+TimeOutPS3 timeOut_Silang(200);
+TimeOutPS3 timeOut_Lingkaran(200);
+TimeOutPS3 timeOut_Kotak(200);
+
+int main()
+{
+    //=========================INITIALIZATION====================================
+    can_kanan.IsAlwaysSend(1);
+    can_kiri.IsAlwaysSend(1);
+
+    ms_tick.attach_us(onMillisecondTicker,1000);
+    
+    led = 1;
+    
+    ps3.setup();
+
+    float vx;
+    float vy;
+    float omega;
+
+    bool isDebug = false;
+    float FL_setvalue, FR_setvalue, BL_setvalue, BR_setvalue;
+
+    float vx_mag = 1;
+    float vy_mag = 1;
+    float omega_mag = 0.7;
+    // float deltaVRight, deltaVLeft;
+    // float deltaVFor, deltaVRev;
+
+    // --------------------------------------------------------------------------
+
+    // ---------------- STORAGE -------------------
+    bool release_red_ball = false;
+    bool release_blue_ball = false;
+    bool separator_extended = false;
+
+    while (true)
+    {
+        ps3.updateMillis(millis);
+        ps3.baca_data();
+        ps3.olah_data();
+
+        //============================DEBUG GAMING==================================
+
+        isDebug = true;
+        if (isDebug)
+        {
+            //printf("RX : %d RY : %d LX : %d LY : %d", ps3.getRX(), ps3.getRY(), ps3.getLX(), ps3.getLY());
+            //printf("R1 : %d R2 : %d L1 : %d L2 : %d", ps3.getR1(), ps3.getR2(), ps3.getL1(), ps3.getL2());
+            printf("Kotak : %d Silang : %d Lingkaran : %d Segitiga : %d\n", ps3.getKotak(), ps3.getSilang(), ps3.getLingkaran(), ps3.getSegitiga());
+            //printf("Up : %d Left : %d Right : %d Down : %d", ps3.getButtonUp(), ps3.getButtonLeft(), ps3.getButtonRight(), ps3.getButtonDown());
+            //printf("Start : %d Select : %d\n", ps3.getStart(), ps3.getSelect());
+            // printf("vx : %f vy : %f omega : %f ", vx, vy, omega);
+            // printf("FL : %f FR : %f BL : %f BR : %f \n", FL_setvalue, FR_setvalue, BL_setvalue, BR_setvalue);
+
+        }
+        // --------------------------------------------------------------------------
+
+        //==========================JOYSTICK PS3 MOVEMENT============================
+               //Reset Value
+        vx = 0;
+        vy = 0;
+        omega = 0;
+        release_blue_ball = false;
+        release_red_ball = false;
+        separator_extended = false;
+
+        if (ps3.getSilang())
+        {
+            if (timeOut_Silang.checkTimeOut(true, millis))
+            {
+                release_blue_ball = true;
+            }
+        }
+        else {
+            timeOut_Silang.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getLingkaran())
+        {
+            if (timeOut_Lingkaran.checkTimeOut(true, millis))
+            {
+                release_red_ball = true;
+            }
+        }
+        else {
+            timeOut_Lingkaran.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getKotak())
+        {
+            if (timeOut_Kotak.checkTimeOut(true, millis))
+            {
+                separator_extended = true;
+            }
+        }
+        else {
+            timeOut_Kotak.checkTimeOut(false, millis);
+        }
+
+         if (ps3.getL1())
+        {
+            if (timeOut_L1.checkTimeOut(true, millis))
+            {
+                omega += omega_mag;
+            }
+        }
+        else {
+            timeOut_L1.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getR1())
+        {
+            if (timeOut_R1.checkTimeOut(true, millis))
+            {
+                omega += -(omega_mag);
+            }
+        }
+        else {
+            timeOut_R1.checkTimeOut(false, millis);
+        }
+        
+        if (ps3.getButtonUp())
+        {
+            if (timeOut_Atas.checkTimeOut(true, millis))
+            {
+                vx += -(vx_mag);
+            }
+        }
+        else {
+            timeOut_Atas.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getButtonDown())
+        {
+            if (timeOut_Bawah.checkTimeOut(true, millis))
+            {
+                vx += vx_mag;
+            }
+        }
+        else {
+            timeOut_Bawah.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getButtonLeft())
+        {
+            if (timeOut_Kiri.checkTimeOut(true, millis))
+            {
+                vy += -(vy_mag);
+            }
+        }
+        else {
+            timeOut_Kiri.checkTimeOut(false, millis);
+        }
+
+        if (ps3.getButtonRight())
+        {
+            if (timeOut_Kanan.checkTimeOut(true, millis))
+            {
+                vy += vy_mag;
+            }
+        }
+        else {
+            timeOut_Kanan.checkTimeOut(false, millis);
+        }
+
+        #define SPEED_MULTIPLIER 1.5f
+        #define OMEGA_MULTIPLIER 1.5f
+
+        vx = SPEED_MULTIPLIER*vx;
+        vy = SPEED_MULTIPLIER*vy;
+        omega = OMEGA_MULTIPLIER*omega;
+
+
+        // OMNIWHEEL
+        omniwheel.setVx(vx);
+        omniwheel.setVy(vy);
+        omniwheel.setOmega(omega);
+
+        omniwheel.InverseCalc();
+
+        // BASE KANAN
+        BR_setvalue = omniwheel.getFLSpeedRPS();
+        FR_setvalue = omniwheel.getBLSpeedRPS();
+
+        // BASE KIRI
+        BL_setvalue = omniwheel.getFRSpeedRPS();
+        FL_setvalue = omniwheel.getBRSpeedRPS();
+
+/*         // Set the motor speeds
+        #define SPEED_MULTIPLIER 1.5f
+        #define OMEGA_MULTIPLIER 1.5f
+
+        sender.setMotor1(int(vx*1000*SPEED_MULTIPLIER));
+        sender.setMotor2(int(vy*1000*SPEED_MULTIPLIER));
+        sender.setInteger(int(omega*1000*OMEGA_MULTIPLIER)); */
+
+        //------------------------------------------------
+        #define ANALOG_SCALE 1000.0f
+
+
+        // CAN KANAN
+        can_kanan.setMotor1(BR_setvalue*ANALOG_SCALE);
+        can_kanan.setMotor2(FR_setvalue*ANALOG_SCALE);
+
+        // CAN KIRI
+        can_kiri.setMotor1(BL_setvalue*ANALOG_SCALE);
+        can_kiri.setMotor2(FL_setvalue*ANALOG_SCALE);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+        // CAN STORAGE
+        storage.setSwitch1(release_blue_ball);
+        storage.setSwitch2(release_red_ball);
+        storage.setSwitch3(separator_extended);
+
+        can_kanan.sendCAN(can_kanan.getNoBM(), TS_SEND_CAN);
+        can_kiri.sendCAN(can_kiri.getNoBM(), TS_SEND_CAN);
+        storage.sendCAN(storage.getNoBM(), TS_SEND_CAN);
+    }
+    return 0;
+}
